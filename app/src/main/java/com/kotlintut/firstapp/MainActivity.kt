e3c8b7db3e755882c314cb51b80a364b63613a90
MHThe1 <mehedihtanvir@gmail.com>
2025-02-26 00:03:59 +0600

    Dialog

--- app/src/main/java/com/kotlintut/firstapp/MainActivity.kt
+++ app/src/main/java/com/kotlintut/firstapp/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.kotlintut.firstapp
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,16 +12,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kotlintut.firstapp.ui.theme.FirstAppTheme
@@ -45,6 +48,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
+    val showDialog = remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -58,79 +62,77 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 .padding(16.dp),
             contentAlignment = Alignment.BottomStart
         ) {
-            Text("LazyColumn",
+            Text("Dialog",
                 style = MaterialTheme.typography.headlineLarge,
                 color = MaterialTheme.colorScheme.onPrimary)
         }
 
         Spacer(modifier = Modifier.height(10.dp))
 
-
-//        SimpleLazyColumn()
-        TaskLazyColumn()
-    }
-}
-
-
-@Composable
-fun SimpleLazyColumn() {
-    LazyColumn {
-        items(40) { i ->
-            Text(
-                text= "Item number $i",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-            )
-
-        }
-    }
-}
-
-
-data class Task(val id: Int, val title: String)
-
-val taskList = listOf(
-    Task(1, "Buy tomato"),
-    Task(2, "Complete quiz prep"),
-    Task(3, "Read a book"),
-    Task(4, "Exercise"),
-    Task(5, "Play some game"),
-    Task(6, "Clean the room"),
-    Task(7, "Watch some tutorials"),
-    Task(8, "Practice android development"),
-    Task(9, "Plan the next day"),
-    Task(10, "Cook some food")
-)
-
-@Composable
-fun TaskLazyColumn() {
-    LazyColumn {
-        items(taskList) { task ->
-            TaskItem(task)
-        }
-    }
-}
-
-@Composable
-fun TaskItem(task: Task) {
-    val context = LocalContext.current
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 8.dp, vertical = 2.dp)
-            .clickable(onClick = {Toast.makeText(context, "Clicked on ${task.title}", Toast.LENGTH_SHORT).show()})
-    ) {
-        Text(
-            text = task.title,
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            style = MaterialTheme.typography.bodyLarge
-        )
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = { showDialog.value = true}
+            ) { Text("Show Dialog") }
+        }
+
+        if(showDialog.value) {
+            AlertDialogExample(
+                onDismissRequest = { showDialog.value = false },
+                onConfirmation = { showDialog.value = false },
+                dialogTitle = "Dialog",
+                dialogText = "This is a sample dialog box description",
+                icon = Icons.Default.Info
+            )
+        }
+
     }
 }
 
+@Composable
+fun AlertDialogExample(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Example des")
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest ()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
