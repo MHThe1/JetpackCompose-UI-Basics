0d38166997350347126e4a11766a7f3ffcef4a99
MHThe1 <mehedihtanvir@gmail.com>
2025-03-06 17:27:13 +0600

    State management

--- app/src/main/java/com/kotlintut/firstapp/MainActivity.kt
+++ app/src/main/java/com/kotlintut/firstapp/MainActivity.kt
@@ -5,28 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kotlintut.firstapp.ui.theme.FirstAppTheme
@@ -48,91 +47,78 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    val showDialog = remember { mutableStateOf(false) }
-
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
     ) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(100.dp)
+                .height(IntrinsicSize.Max)
                 .background(MaterialTheme.colorScheme.primary)
                 .padding(16.dp),
-            contentAlignment = Alignment.BottomStart
+            contentAlignment = Alignment.CenterStart
         ) {
-            Text("Dialog",
+            Text("State Management",
                 style = MaterialTheme.typography.headlineLarge,
                 color = MaterialTheme.colorScheme.onPrimary)
         }
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Button(
-                onClick = { showDialog.value = true}
-            ) { Text("Show Dialog") }
-        }
-
-        if(showDialog.value) {
-            AlertDialogExample(
-                onDismissRequest = { showDialog.value = false },
-                onConfirmation = { showDialog.value = false },
-                dialogTitle = "Dialog",
-                dialogText = "This is a sample dialog box description",
-                icon = Icons.Default.Info
-            )
-        }
+        NameScreen()
 
     }
 }
 
+
+
 @Composable
-fun AlertDialogExample(
-    onDismissRequest: () -> Unit,
-    onConfirmation: () -> Unit,
-    dialogTitle: String,
-    dialogText: String,
-    icon: ImageVector,
-) {
-    AlertDialog(
-        icon = {
-            Icon(icon, contentDescription = "Example des")
-        },
-        title = {
-            Text(text = dialogTitle)
-        },
-        text = {
-            Text(text = dialogText)
-        },
-        onDismissRequest = {
-            onDismissRequest ()
-        },
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    onConfirmation()
-                }
-            ) {
-                Text("Confirm")
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = {
-                    onDismissRequest()
-                }
-            ) {
-                Text("Dismiss")
-            }
-        }
-    )
+fun NameScreen() {
+
+    var name by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        NameInput(name = name, onNameChange = { name = it })
+        Greeting(name = name)
+    }
 }
+
+@Composable
+fun NameInput(name: String, onNameChange: (String) -> Unit) {
+
+    var tempName by rememberSaveable { mutableStateOf(name) }
+
+    TextField(
+        value = tempName,
+        onValueChange = { tempName = it },
+        label = { Text("Enter your name")}
+    )
+    Button(
+        modifier = Modifier
+            .padding(top = 10.dp),
+        onClick = { onNameChange(tempName)}) {
+        Text("Confirm Name")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    if(name.isNotEmpty()){
+        Text(
+            text = "Hi $name!",
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.displayLarge
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
