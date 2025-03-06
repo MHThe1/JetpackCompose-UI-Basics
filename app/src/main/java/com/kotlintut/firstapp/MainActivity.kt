d18a0b40c8411de316362ef7c82bebf64963cf9e
MHThe1 <mehedihtanvir@gmail.com>
2025-03-06 18:46:18 +0600

    Snackbar

--- app/src/main/java/com/kotlintut/firstapp/MainActivity.kt
+++ app/src/main/java/com/kotlintut/firstapp/MainActivity.kt
@@ -15,13 +15,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -29,6 +36,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kotlintut.firstapp.ui.theme.FirstAppTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,7 +67,7 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 .padding(16.dp),
             contentAlignment = Alignment.CenterStart
         ) {
-            Text("State Management",
+            Text("SnackBar",
                 style = MaterialTheme.typography.headlineLarge,
                 color = MaterialTheme.colorScheme.onPrimary)
         }
@@ -70,6 +78,31 @@ fun MainScreen(modifier: Modifier = Modifier) {
 
     }
 }
+
+
+//@Composable
+//fun SnackBarExample() {
+//    val snackbarHostState = remember { SnackbarHostState() }
+//    val coroutineScope = rememberCoroutineScope()
+//
+//    Column (
+//        modifier = Modifier
+//            .padding(16.dp)
+//            .fillMaxWidth(),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center
+//    ) {
+//        Button(onClick = {
+//            coroutineScope.launch {
+//                snackbarHostState.showSnackbar("This is a snackbar!")
+//            }
+//        }) {
+//            Text("Show Snackbar")
+//        }
+//    }
+//    SnackbarHost(hostState = snackbarHostState)
+//}
+
 
 
 
@@ -95,17 +128,44 @@ fun NameInput(name: String, onNameChange: (String) -> Unit) {
 
     var tempName by rememberSaveable { mutableStateOf(name) }
 
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
     TextField(
         value = tempName,
         onValueChange = { tempName = it },
-        label = { Text("Enter your name")}
+        label = { Text("Enter your name") }
     )
     Button(
         modifier = Modifier
             .padding(top = 10.dp),
-        onClick = { onNameChange(tempName)}) {
+        onClick = { onNameChange(tempName) }) {
         Text("Confirm Name")
     }
+
+    if(name.isNotEmpty()) {
+        Button(onClick = {
+            onNameChange("")
+            coroutineScope.launch {
+                val result = snackbarHostState.showSnackbar(
+                    message = "Name deleted!",
+                    actionLabel = "Undo",
+                    duration = SnackbarDuration.Short
+                )
+                if(result == SnackbarResult.ActionPerformed) {
+                    onNameChange(name)
+                }
+            }
+        },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.error
+            )) {
+            Text("Delete name!")
+        }
+    }
+
+    SnackbarHost(hostState = snackbarHostState)
+
 }
 
 @Composable
@@ -122,7 +182,7 @@ fun Greeting(name: String) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ScreenPreview() {
     FirstAppTheme {
         MainScreen()
     }
